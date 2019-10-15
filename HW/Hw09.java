27b6eee60c60831f1be335357b7f67bc3a9988d1
kijongjo <cozacoza12@naver.com>
2019-10-15 14:57:12 +0900

    1111

--- HW/Hw09.java
+++ HW/Hw09.java
@@ -2,12 +2,23 @@ package day03;
 
 public class Hw09 {
 	public static void main(String[] args) {
-		for (int i = 1; i <=5; i++) {
-			
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5; i <= j; j--) {
+
 				System.out.print("*");
-			
+			}
+
 			System.out.println();
 		}
+
+ for (int i = 0; i < args.length; i++) {
+	
+}		
+                
+			
+                 
+		
+
 	}
 
 }
